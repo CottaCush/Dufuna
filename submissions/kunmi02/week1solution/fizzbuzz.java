a9a4ced25d4a7eb98293b028437569f8cb34a2ac
kunmi02 <mrkhunmie@gmail.com>
2018-06-11 04:38:29 +0100

    I have added the suggested chnages to the file

--- submissions/kunmi02/week1solution/fizzbuzz.java
+++ submissions/kunmi02/week1solution/fizzbuzz.java
@@ -1,24 +1,21 @@
-
-public class Fizzbuzz {
-
-    
+public class Fizzbuzz {    
     public static void main(String[] args) {
         // TODO code application logic here
         int endcount=100; //end of my counter
         for (int i=1;i<=endcount;i++){ //initialized a for loop to count from 1 - 100
             
             if ((i % 3)==0 && (i % 5)==0){ // condition for fizzbuzz
-                System.out.println ("FizzBuzz" + "\n");
-            } // end of fizzbuzz
+                System.out.println ("FizzBuzz" );
+            } 
             else if ((i % 3)==0){ //condition fizz
-                System.out.println ("Fizz" + "\n");
-            } //end of fizz
+                System.out.println ("Fizz" );
+            } 
             else if ((i % 5)==0){ // condition for buzz
-                System.out.println ("Buzz" + "\n");
-            } // eond of buzz
+                System.out.println ("Buzz" );
+            } 
             else{
-            System.out.println ( i + "\n"); //print the number if it does not satisfy the fizz,buzz or fizzbuzz condition
-        }
+            System.out.println ( i ); //print the number if it does not satisfy the fizz,buzz or fizzbuzz condition
+            }
         }
     }
     
