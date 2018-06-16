bb7565fbf7930d057122bda0431493dbc23ef474
Esebame1 <37189955+Esebame1@users.noreply.github.com>
2018-06-16 02:33:27 +0100

    Update Week_1_solution.py

--- submissions/Esebame1/Week_1_solution.py
+++ submissions/Esebame1/Week_1_solution.py
@@ -1,11 +1,9 @@
 for a in range (1,100):
   if a%3==0 and a%5==0:
     print('Fizzbuzz\n')
-     continue
   elif a%3==0:
     print('Fizz\n')
-     continue
   elif a%5==0:
     print('Buzz\n')
-     continue
+     else:
   print (str(a)+'\n')
